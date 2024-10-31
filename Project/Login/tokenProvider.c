5e4299cdcf9ec4c597160a9a3b99a3680460d2b9
Jaemin <ghdckdwn8456@gmail.com>
2024-10-31 13:43:19 +0900

    [fix] tokenProvider 반환값 수정

--- Project/Login/tokenProvider.c
+++ Project/Login/tokenProvider.c
@@ -4,7 +4,9 @@
 #include <jwt.h>
 #include "tokenProvider.h"
 
-char* MakeAccessToken(const char* email, const char* uid, const char* encryptKey)
+#define ISS "IUCyH"
+
+char* MakeAccessToken(const char* uid, const char* encryptKey)
 {
 	jwt_t* jwt = NULL;
 	time_t now = time(NULL);
@@ -12,9 +14,8 @@ char* MakeAccessToken(const char* email, const char* uid, const char* encryptKey
 
 	jwt_new(&jwt);
 
-	jwt_add_grant(jwt, "email", email);
 	jwt_add_grant(jwt, "sub", uid);
-	jwt_add_grant(jwt, "iss", "protectmeios.xyz");
+	jwt_add_grant(jwt, "iss", ISS);
 	
 	jwt_add_grant_int(jwt, "iat", now);
 	jwt_add_grant_int(jwt, "exp", now + thirtyMinutes);
@@ -33,9 +34,8 @@ char* MakeRefreshToken(const char* email, const char* uid, const char* encryptKe
 
 	jwt_new(&jwt);
 
-	jwt_add_grant(jwt, "email", email);
 	jwt_add_grant(jwt, "sub", uid);
-	jwt_add_grant(jwt, "iss", "protectmeios.xyz");
+	jwt_add_grant(jwt, "iss", ISS);
 	
 	jwt_add_grant_int(jwt, "iat", now);
 	jwt_add_grant_int(jwt, "exp", now + oneMonth);
@@ -43,5 +43,5 @@ char* MakeRefreshToken(const char* email, const char* uid, const char* encryptKe
 	jwt_set_alg(jwt, JWT_ALG_HS256, (unsigned char*)encryptKey, strlen(encryptKey));
 	
 	char* refreshToken = jwt_encode_str(jwt);
-	return accessToken;
+	return refreshToken;
 }
